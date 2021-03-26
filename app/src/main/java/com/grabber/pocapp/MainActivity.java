afa36b0d23f6f114281e7a8c51ab2f7a28787c89
kry-p <jhjung.dev@gmail.com>
2021-03-26 19:45:46 +0900

    Implement TabLayout and ViewPager with mock fragments
    모의 Fragment가 표시될 TabLayout과 스크롤 구현을 위한 ViewPager를 구현하였습니다.

--- app/src/main/java/com/grabber/pocapp/MainActivity.java
+++ app/src/main/java/com/grabber/pocapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.grabber.pocapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager=findViewById(R.id.viewPager);
+        TabLayout tabLayout=findViewById(R.id.pageTab);
+        VPAdapter adapter=new VPAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/grabber/pocapp/VPAdapter.java
@@ -0,0 +1,34 @@
+package com.grabber.pocapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.grabber.pocapp.fragments.HistoryFragment;
+import com.grabber.pocapp.fragments.PropertyFragment;
+
+import java.util.ArrayList;
+
+public class VPAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> items;
+
+    public VPAdapter(FragmentManager fm) {
+        // noinspection deprecation
+        super(fm);
+        items = new ArrayList<>();
+        items.add(PropertyFragment.newInstance());
+        items.add(HistoryFragment.newInstance());
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+}
