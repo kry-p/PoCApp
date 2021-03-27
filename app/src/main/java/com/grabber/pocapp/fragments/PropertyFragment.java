68aa8d1973669e876e073aee9f578962edcd8a5b
JaeMinKim <sxbgdcvf@naver.om>
2021-03-27 18:11:16 +0900

    Implement pie chart

--- app/src/main/java/com/grabber/pocapp/fragments/PropertyFragment.java
+++ app/src/main/java/com/grabber/pocapp/fragments/PropertyFragment.java
@@ -1,5 +1,6 @@
 package com.grabber.pocapp.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,7 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.grabber.pocapp.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +27,8 @@ import com.grabber.pocapp.R;
  * create an instance of this fragment.
  */
 public class PropertyFragment extends Fragment {
+
+    PieChart pieChart;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -52,12 +65,66 @@ public class PropertyFragment extends Fragment {
 //            mParam1 = getArguments().getString(ARG_PARAM1);
 //            mParam2 = getArguments().getString(ARG_PARAM2);
 //        }
+
+
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_property, container, false);
+        View v= inflater.inflate(R.layout.fragment_property, container, false);
+
+        pieChart = (PieChart)v.findViewById(R.id.piechart);
+
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5,10,5,5);
+
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+
+        pieChart.setDrawHoleEnabled(false);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setTransparentCircleRadius(61f);
+
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+
+
+        yValues.add(new PieEntry(23f,"USA"));
+        yValues.add(new PieEntry(14f,"UK"));
+        yValues.add(new PieEntry(35f,"India"));
+        yValues.add(new PieEntry(40f,"Russia"));
+        yValues.add(new PieEntry(40f,"Korea"));
+
+
+        Description description = new Description();
+
+//        for(PieEntry i : yValues) {
+//            description.setText(i.getLabel());
+//            //description.setText("세계 국가"); //라벨
+//            description.setTextSize(15);
+//            pieChart.setDescription(description[0]);
+//        }
+
+        description.setText("세계 국가"); //라벨
+        description.setTextSize(15);
+        pieChart.setDescription(description);
+
+        pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic); //애니메이션
+
+        PieDataSet dataSet = new PieDataSet(yValues,    "Countries");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        PieData data = new PieData((dataSet));
+        data.setValueTextSize(10f);
+        data.setValueTextColor(Color.YELLOW);
+
+        pieChart.setData(data);
+
+        return v;
     }
 }
